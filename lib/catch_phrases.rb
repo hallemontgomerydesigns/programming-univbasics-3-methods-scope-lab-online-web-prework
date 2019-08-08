1ff2f3a9075fe01873965890136551b000b17667
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-08-08 16:09:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,7 @@
-
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
